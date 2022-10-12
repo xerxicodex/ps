9bf29121294bbef99578c981986b8d408fc27512
jamezilla <jamezilla>
2022-10-12 18:31:15 -0400

    auth routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,63 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import type { AppProps } from 'next/app';
+import { withTRPC } from '@trpc/next';
+import type { AppRouter } from '../server/routers/app.routes';
+import { loggerLink } from '@trpc/client/links/loggerLink';
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
+import superjson from 'superjson';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='bg-slate-700 select-none'>
+      <ToastContainer />
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </div>
+  );
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : (process.env.NEXT_PUBLIC_TRPC_ENDPOINT as string);
+
+    const links = [
+      loggerLink(),
+      httpBatchLink({
+        maxBatchSize: 10,
+        url,
+      }),
+    ];
+    return {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 1000,
+          },
+        },
+      },
+      headers() {
+        if (ctx?.req) {
+          return {
+            ...ctx.req.headers,
+            'x-ssr': '1',
+          };
+        }
+        return {};
+      },
+      links,
+      transformer: superjson,
+      fetch(url, options) {
+        return fetch(url, {
+          ...options,
+          credentials: 'include',
+        });
+      },
+    };
+  },
+  ssr: false,
+})(MyApp);
--- server/routers/app.routes.ts
+++ server/routers/app.routes.ts
@@ -1,5 +1,8 @@
 import { createRouter } from '../createRouter';
 import redisClient from '../utils/connectRedis';
+import authRouter from './auth.routes';
+import userRouter from './user.routes';
+
 import connectDB from '../utils/prisma';
 
 // Connect to Prisma
@@ -9,11 +12,14 @@ export const appRouter = createRouter().query('ping', {
     resolve: async (ctx) => {
       return "pong"
     },
-  }).query('hello', {
-  resolve: async (ctx) => {
-    const message = await redisClient.get('tRPC');
-    return { message };
-  },
-});
+  })
+  .query('hello', {
+    resolve: async (ctx) => {
+      const message = await redisClient.get('tRPC');
+      return { message };
+    },
+  })
+  .merge('auth.', authRouter)
+  .merge('users.', userRouter);
 
 export type AppRouter = typeof appRouter;
